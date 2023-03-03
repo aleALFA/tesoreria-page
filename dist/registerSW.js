2ee6513888e82364b9f218e3e1e7cf1e643aa2a3
aleALFA <Jesus.Alvarez2@AB-inbev.com>
2023-03-03 12:56:20 -0600

    feat: resolve security issues

--- dist/registerSW.js
+++ dist/registerSW.js
@@ -1,1 +1,1 @@
-if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/sw.js', { scope: '/' })})}
+if('serviceWorker' in navigator) {window.addEventListener('load', () => {navigator.serviceWorker.register('/core-tesoreria-page/sw.js', { scope: '/core-tesoreria-page/' })})}
